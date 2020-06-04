32172a388eef44c15c2ee3afd7c03400918ed1e1
Alfred <appiagyeialfred815@gmail.com>
2020-06-04 11:14:58 +0000

    add email

--- src/components/Home.js
+++ src/components/Home.js
@@ -921,7 +921,7 @@ class Home extends Component {
   <div class="container-fluid"  id="footer-container"> 
   <div class="row" id="contacts">
             <h3 style={{color:"#fff"}}>Contact us</h3>
-            <span><i class="fa fa-envelope"> Info@name.com</i></span>
+            <span><i class="fa fa-envelope"><a href="mailto:appiagyeialfred815@gmail.com">soccerangels@email.com</a></i></span>
             <span><i class="fa fa-envelope">Nname@domain.com</i></span>
             <span><i class="fa fa-phone"></i>#########/#######</span>
             
