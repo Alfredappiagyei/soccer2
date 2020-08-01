8f6184c2ad474900c0bc96e5ebf385eb9b8805df
Alfred <appiagyeialfred815@gmail.com>
2020-08-01 09:12:44 +0000

    m

--- src/components/Features.js
+++ src/components/Features.js
@@ -5,106 +5,92 @@ import soccerHomeimage1 from '../soccer/photos/soccer1.JPG'
 class Features extends Component {
     render () {
         return(
-            <div>
-                  <div class="container-fluid">
+            
+
+ 
+
+
+     
+         <section class="container-fluid">
+         <div className="row">
+              <Link to="#"> <h2><b>Features</b></h2></Link>                   
+              </div>
+   
             <div class="row" id="features_mainRows">
-               <a href="#" id="features_header_text">
-                    <h3><b>FEATURES ></b></h3>
-                </a>
-
                 <div class="col-md-6">
-                    <div class="row" id="features_subRows">
-                        <div class="col-md-12 col-xs-12">
-                            <a href="#"> <img src={soccerHomeimage1} alt="pizza"
-                                class="img-responsive" /></a>
+                    <div class="row">
+                        <div class="col-md-12 col-xs-12" style={{padding:"40px"}}>
+                            <a href="#"> <img src={jerseyimage1} alt="pizza"
+                                class="img-responsive"/></a>
+                                <div className="line"></div>
                         </div>
                         <div class="col-md-12 col-xs-12">
-                            <a href="#" id="transfare_news_main_headline">
-                                <p>
-                                    <b>
-                                        Lorem ipsum dolor sit amet,  adipisicing elit, sed do eiusmod
-                                        
-                                </b>
-                                </p>
-                            </a>
+                             <h4 className=" features_main_headline"> Soccer Angels new home kit </h4>
                         </div>
                     </div>
                 </div>
 
+
                 <div class="col-md-6">
                     <div class="row" id="features_subRows">
                         <div class="col-md-4 col-xs-5">
-                            <a href="#"> <img src={soccerHomeimage1} alt="pizza"
+                            <a href="#"> <img src={jerseyimage2} alt="pizza"
                                 class="img-responsive" /></a>
                         </div>
-                        <div class="col-md-8 col-xs-7">
-                            <a href="#" id="transfare_news_sub_headline">
-                                <p>
-                                    <b>
-                                        Lorem ipsum dolor sit amet,  adipisicing elit, sed do eiusmod
-                                        
-                                </b>
-                                </p>
-                            </a>
+                        <div class="col-md-8 col-xs-7 ">
+                                <p className="features_sub_headline">     
+                                        Soccer Angels men's tracksuit                                       
+                                </p>    
                         </div>
                     </div>
 
                     <div class="row" id="features_subRows">
                         <div class="col-md-4 col-xs-5">
-                            <a href="#"> <img src={soccerHomeimage1} alt="pizza"
+                            <a href="#"> <img src={jerseyimage5} alt="pizza"
                                 class="img-responsive" /></a>
                         </div>
                         <div class="col-md-8 col-xs-7">
-                            <a href="#" id="transfare_news_sub_headline">
-                                <p>
-                                    <b>
-                                        Lorem ipsum dolor sit amet, adipisicing elit, sed do eiusmod
-                                        
-                                </b>
-                                </p>
-                            </a>
+                            <p className="features_sub_headline">     
+                                Soccer Angels away jersey kit                                             
+                           </p>                         
                         </div>
                     </div>
 
                     <div class="row" id="features_subRows">
                         <div class="col-md-4 col-xs-5">
-                            <a href="#"> <img src={soccerHomeimage1} alt="pizza"
+                            <a href="#"> <img src={jerseyimage3} alt="pizza"
                                 class="img-responsive" /></a>
                         </div>
                         <div class="col-md-8 col-xs-7">
-                            <a href="#" id="transfare_news_sub_headline">
-                                <p>
-                                    <b>
-                                        Lorem ipsum dolor sit amet,adipisicing elit, sed do eiusmod
-                                        
-                                </b>
-                                </p>
-                            </a>
+                           <p className="features_sub_headline">     
+                               Soccer angels goalkeeper's training kit                                            
+                           </p> 
+                            
                         </div>
                     </div>
-
+                   
+                    
                     <div class="row" id="features_subRows">
                         <div class="col-md-4 col-xs-5">
-                            <a href="#"> <img src={soccerHomeimage1} alt="pizza"
+                            <a href="#"> <img src={jerseyimage4} alt="pizza"
                                 class="img-responsive" /></a>
                         </div>
                         <div class="col-md-8 col-xs-7">
-                            <a href="#" id="transfare_news_sub_headline">
-                                <p>
-                                    <b>
-                                        Lorem ipsum dolor sit amet,  adipisicing elit, sed do eiusmod
-                                        
-                                </b>
-                                </p>
-                            </a>
+                           <p className="features_sub_headline">     
+                              Soccer Angels shake up kit                                           
+                           </p> 
+                            
                         </div>
-                    </div>
-
+                    </div> 
                 </div>
+            </div>
+        </section>
+ 
 
-            </div>
-        </div>
-            </div>
+
+
+
+
         )
     }
 }
