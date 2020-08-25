99c6986e07631437bbbb90ae2e34f2d7d1ef5545
Alfred <appiagyeialfred815@gmail.com>
2020-08-25 11:44:38 +0000

    change background color of SAFA TV

--- src/components/Fans/SeatvGo.js
+++ src/components/Fans/SeatvGo.js
@@ -11,12 +11,18 @@ import Footer from "../Footer";
 class SocialMedia extends Component {
     render() {           
         return (
-          <div>
-           <div className="line"></div>
-                <section>
+          <div  style={{background:"green"}}>
+           <div className="line">   </div>
+           <div className="container"  style={{color:"lawngreen"}}>
+           <h3>SAFA TV GO</h3>
+           </div>
+           <div className="container">
+           <section>
                     <iframe width="100%" height="500" title="SAFA" src="https://www.youtube.com/embed/videoseries?list=PL2yC1-VwXuLZCsx-qeHLeGbO_pt9zZ3f0&rel=0"  frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
                       </iframe>
                 </section>
+           </div>
+               
             <div className="line"></div>
     
                   <footer>
