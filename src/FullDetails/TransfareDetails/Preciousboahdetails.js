351ce328db7126119f5cda52bca53536d17204ee
Alfred <appiagyeialfred815@gmail.com>
2020-08-27 11:01:13 +0000

    add social links to boah details section

--- src/FullDetails/TransfareDetails/Preciousboahdetails.js
+++ src/FullDetails/TransfareDetails/Preciousboahdetails.js
@@ -2,17 +2,19 @@ import React, {Component} from "react";
 import precious from "../photos/precious.jpg";
 import precious2 from "../photos/precious2.JPG";
 import jerseygrid from "../photos/0.gif";
-// import {
+import soccerHomeimage1 from "../photos/soccer1.JPG";
+ import {
 
-//     // BrowserRouter as Router,
-//     // Switch,
-//     // Route,
-//     Link
+     // BrowserRouter as Router,
+     // Switch,
+     // Route,
+     Link
   
-//   } from "react-router-dom";
+   } from "react-router-dom";
 
 
 import Footer from "../../components/Footer";
+import SocialHeader from "../../components/SocilaHeader";
 
 class BoahDetails extends Component {
     render () {
@@ -26,7 +28,11 @@ class BoahDetails extends Component {
                       Academy will recognise truth about his trajectory
                       </h1>                     
                   </div>
+
                <div className="container">
+               <div className="row">
+                      <SocialHeader/>
+                  </div>
                    <div className="row">
                <div className="col-md-9">
                    <div className="detailsImage">
@@ -65,13 +71,108 @@ class BoahDetails extends Component {
                </div>
               
                  <div className="col-md-3">
-                     <div className='row'>
+             <div className='row'>
                      <img
                   src={jerseygrid}
                   alt="jersaecollection"
                   class="img-responsive"
                 />
                      </div>
+                     <div className="recomended">
+                         <h4><b>RECOMENDED</b></h4>
+                     </div>
+                     <hr/>
+
+                <div class="row" id="features_subRows">
+                    <div class="col-md-6 col-xs-5">
+                  <Link to="#">
+                    {" "}
+                    <img
+                      src={soccerHomeimage1}
+                      alt="pizza"
+                      class="img-responsive"
+                    />
+                  </Link>
+                </div>
+                <div class="col-md-6 col-xs-7 details-tranfare">
+                  <Link to="#">
+                    <p>
+                      Soccer Angels has officcially signed Mustapha Osman from
+                      Lakers fc
+                    </p>
+                  </Link>
+                </div>
+              </div>
+                <hr/>
+
+
+              <div class="row" id="features_subRows">
+                    <div class="col-md-6 col-xs-5">
+                  <Link to="#">
+                    {" "}
+                    <img
+                      src={soccerHomeimage1}
+                      alt="pizza"
+                      class="img-responsive"
+                    />
+                  </Link>
+                </div>
+                <div class="col-md-6 col-xs-7  details-tranfare">
+                  <Link to="#">
+                    <p>
+                      Soccer Angels has officcially signed Mustapha Osman from
+                      Lakers fc
+                    </p>
+                  </Link>
+                </div>
+              </div>
+              <hr/>
+
+              <div class="row" id="features_subRows">
+                    <div class="col-md-6 col-xs-6">
+                  <Link to="#">
+                    {" "}
+                    <img
+                      src={soccerHomeimage1}
+                      alt="pizza"
+                      class="img-responsive"
+                    />
+                  </Link>
+                </div>
+                <div class="col-md-6 col-xs-6  details-tranfare">
+                  <Link to="#">
+                    <p>
+                      Soccer Angels has officcially signed Mustapha Osman from
+                      Lakers fc
+                    </p>
+                  </Link>
+                </div>
+              </div>
+              <hr/>
+
+
+              <div class="row" id="features_subRows">
+                    <div class="col-md-6 col-xs-5">
+                  <Link to="#">
+                    {" "}
+                    <img
+                      src={soccerHomeimage1}
+                      alt="pizza"
+                      class="img-responsive"
+                    />
+                  </Link>
+                </div>
+                <div class="col-md-6 col-xs-7  details-tranfare">
+                  <Link to="#">
+                    <p>
+                      Soccer Angels has officcially signed Mustapha Osman from
+                      Lakers fc
+                    </p>
+                  </Link>
+                </div>
+              </div>
+              
+
                  </div>
                  </div>
                </div>
---
+++ src/components/SocilaHeader.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import footlogo from './Fans/photos/footlogo.JPG'
+
+
+
+ 
+// import {
+
+//     // BrowserRouter as Router,
+//     // Switch,
+//     // Route,
+//     Link
+  
+//   } from "react-router-dom";
+class SocialHeader extends Component {
+    render() {
+
+        return (
+            <div>
+            <hr/>
+              <div className="socialHead">
+                          <span><h3>SOCCER ANGELS NEWS</h3></span>  
+                          <span className="socialink"><a href="https://twitter.com/SoccerAngelAca1"><i class="fa fa-twitter"></i></a>|</span> 
+                          <span className="socialink"><a href="https://web.facebook.com/Soccer-Angels-Academy-115197873244725/"><i class="fa fa-facebook"></i></a>|</span> 
+                          <span className="socialink"><a  href="https://www.youtube.com/channel/UC6AD72yTtZpE7ox25eb6GZg" ><i class="fa fa-youtube"></i></a>|</span> 
+                          <span className="socialink"><a href="https://www.instagram.com/p/CAIWNzvF676/"><i class="fa fa-instagram"></i></a>|</span>  
+                      
+                </div> 
+                <hr/>
+                </div>
+        )
+    }
+}
+
+export default SocialHeader;
